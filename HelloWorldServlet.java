8ea6e543b15992e5c9b04f449a48aed49d4b43a0
Karthikeyan2420 <84924959+Karthikeyan2420@users.noreply.github.com>
2024-09-09 19:46:57 +0530

    Add files via upload

--- HelloWorldServlet.java
+++ HelloWorldServlet.java
@@ -2,6 +2,11 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +32,7 @@ public class HelloWorldServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // Set the content type to HTML
+    	  // Set the content type to HTML
         response.setContentType("text/html");
 
         // Get the PrintWriter object to write the response
@@ -39,8 +44,28 @@ public class HelloWorldServlet extends HttpServlet {
         out.println("<body>");
         out.println("<h1>Hello, World!</h1>");
         out.println("<p>Welcome to the Java Servlet!</p>");
-        out.println("</body>");
-        out.println("</html>");
+        
+    	 try {
+    		
+    		 Class.forName("oracle.jdbc.driver.OracleDriver");
+    		 
+             Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "system", "oracle");
+            
+             out.println("<h1>something</h1>");
+             PreparedStatement stmt = con.prepareStatement("select * from t3");
+             ResultSet rs = stmt.executeQuery();
+             
+             out.println("<br>");
+             while (rs.next()) {
+                 out.println(rs.getInt(1) + " " + rs.getString(2) );
+                 out.println("<br>");
+             }
+         } catch (ClassNotFoundException | SQLException e) {
+             e.printStackTrace();
+         }
+    	 out.println("</body>");
+         out.println("</html>");
+
     }
 
     /**
